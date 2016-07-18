6fae27ce1f7da15b3a7e28a0ec25349be58abc25
sw4655315 <as4655315@163.com>
2016-07-18 09:31:29 +0800

    init

---
+++ www/js/app.js
@@ -0,0 +1,31 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+var ionicapp = angular.module('starter', ['ionic', 'starter.controllers','starter.kit']);
+Mock.mockjax(ionicapp);
+ionicapp.run(function($ionicPlatform,$rootScope,$ionicLoading) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+  $rootScope.loading = function(){
+    $ionicLoading.show({template: '加载中...'});
+  }
+  $rootScope.loaded = function(){
+    $ionicLoading.hide();
+  }
+})
+
+;
---
+++ www/js/controllersIndex.js
@@ -0,0 +1,197 @@
+angular.module('starter.controllersIndex', [])
+
+.controller('index', function($scope, $ionicModal, $timeout) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+.controller('AppCtrl', function($scope, $ionicModal, $timeout,$state,$ionicSideMenuDelegate) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.loginModal = modal;
+  });
+  $ionicModal.fromTemplateUrl('templates/search.html', {
+    scope: $scope
+  }).then(function(searchModal) {
+    $scope.searchModal = searchModal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.loginModal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.loginModal.show();
+  };
+
+
+    // Triggered in the login modal to close it
+  $scope.closeSearch = function() {
+    $scope.searchModal.hide();
+  };
+
+  // Open the login modal
+  $scope.search = function() {
+    $scope.searchModal.show();
+  };
+
+    // Open the login modal
+  $scope.doSearch = function() {
+    $timeout(function() {
+      $scope.searchModal.hide();
+    }, 400);
+    $ionicSideMenuDelegate.toggleLeft(false);
+    $state.go('app.playlists',{'a':1},{'b':2});
+  };
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+    $scope.closeLogin();
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    // $timeout(function() {
+    //   $scope.closeLogin();
+    // }, 1000);
+  };
+})
+
+.controller('PlaylistsCtrl', function($scope,$rootScope,$kit,$stateParams) {
+  $scope.paras = $stateParams;
+  $scope.pLists = [];
+  $scope.loadOver = false;
+  $scope.load = function(){
+    $kit.autoPost('/product/promote',{a:1},function(json){
+      $scope.pLists = $scope.pLists.concat(json.pLists);
+      $scope.page = json.page;
+      $scope.$broadcast('scroll.refreshComplete');
+      $scope.$broadcast('scroll.infiniteScrollComplete'); 
+    })
+  }
+  $scope.reload = function(){
+    $scope.pLists = [];
+    $scope.load();
+  }
+
+  $scope.getNext = function(_index){
+    return $scope.pLists[(_index + 1)];
+  }
+  $scope.onTap = function(){
+    alert(123);
+  }
+  $scope.load();
+
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+  $scope.paras = $stateParams;
+})
+
+.controller('personalCtrl', function($scope, $stateParams) {
+  $scope.paras = $stateParams;
+})
+
+.controller('settingCtrl', function($scope, $stateParams) {
+  $scope.paras = $stateParams;
+})
+
+.controller('searchCtrl', function($scope, $stateParams) {
+  $scope.paras = $stateParams;
+})
+.controller('detailCtrl', function($scope, $stateParams) {
+  $scope.paras = $stateParams;
+})
+.controller('accountCtrl', function($scope, $stateParams,$ionicActionSheet,$kit) {
+  $scope.user = {sex:'请选择'};
+  $scope.choseSex = function(){
+    // 显示操作表
+   $ionicActionSheet.show({
+     buttons: [
+       { text: '男' },
+       { text: '女' },
+     ],
+     titleText: '选择性别',
+     buttonClicked: function(index) {
+       switch(index){
+        case 0:
+          $scope.user.sex="男";break;
+        case 1:
+          $scope.user.sex="女";break;
+       }
+       return true;
+     }
+   });
+  }
+  $scope.paras = $stateParams;
+})
+.controller('addressCtrl', function($scope, $stateParams) {
+  $scope.paras = $stateParams;
+})
+.controller('addressEditCtrl', function($scope, $stateParams) {
+  $scope.paras = $stateParams;
+})
+.controller('orderListCtrl', function($scope, $stateParams) {
+  $scope.paras = $stateParams;
+  // switch ($stateParams.op) {
+  //   case 'all':
+  //     // statements_1
+  //     break;
+  //   case '123':
+  //     // statements_1
+  //     break;
+  //   default:
+  //     // statements_def
+  //     break;
+  // }
+
+})
+;
